ad5bbe8b32347d8fb1f38705789c13bd0afc6ef7
Samuel Ridenour <samrdnr11@gmail.com>
2022-10-15 18:25:33 -0700

    Added eventlistener on mouseenter

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const maxRnC = 16;
 const container = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < maxRnC; i++){
 let row = document.createElement('div');
 row.classList.add('row');
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < maxRnC; j++){
         let column = document.createElement('div');
-        column.classList.add('column');
+        column.classList.add('column');  
+        column.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = 'gray';
+        });
+              
         row.appendChild(column);
     }   
 container.appendChild(row);
